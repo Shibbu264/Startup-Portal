3877d8411a6cd5322e95d84e50449b6c9fa61be2
Shibbu264 <116422972+Shibbu264@users.noreply.github.com>
2024-02-26 23:19:05 +0530

    Update auth.js

--- nextdjan/src/pages/api/auth.js
+++ nextdjan/src/pages/api/auth.js
@@ -1,7 +1,7 @@
 
 import axios from 'axios';
 
-const API_BASE_URL = process.env.API_BASE_URL ;
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL ;
 
 console.log(API_BASE_URL)
 export const login = async (username, password) => {
